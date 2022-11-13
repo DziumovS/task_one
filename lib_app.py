4507ef5295fc0e7224ede423c25952a27022a834
DziumovS <xbartilaxx@gmail.com>
2022-11-13 22:38:45 +0300

    code refactoring again + bp + separate funcs + author[DELETE] update

--- lib_app.py
+++ lib_app.py
@@ -1,11 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
+from app import bp as api_bp
 
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+app.register_blueprint(api_bp, url_prefix='/api')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-from app import routes
