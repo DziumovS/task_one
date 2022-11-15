e8c2fbc1496d60e2ca0b6114c4ba373d22e1eb5e
DziumovS <xbartilaxx@gmail.com>
2022-11-15 21:47:10 +0300

    code refactoring again

--- app/functions.py
+++ app/functions.py
@@ -14,24 +14,3 @@ def is_exists(table: str, id: int=None, name: str=None, surname: str=None):
 
 
 
-
-#
-#
-#
-# def create_sql_statement(table, fields="*", conditions=None, skip=None, offset=None):
-#     sql_statement = f"""SELECT {fields} FROM {table}"""
-#     if conditions:
-#         sql_statement += f""" {conditions}"""  # e.g.: WHERE x == 1, etc.
-#     if skip:
-#         sql_statement += f""" SKIP {skip}"""
-#     if offset:
-#         sql_statement += f""" OFFSET {offset}"""
-#
-#     sql_statement += ";"
-#
-#     return sql_statement
-#
-# sql_statement = create_sql_statement("authors", fields="author.id, author.books", conditions="WHERE id == 2",
-#                                      skip=20, offset=20)
-# # или
-# sql_statement = create_sql_statement("authors")  # получить всех авторов
